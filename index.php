e6c0777a4a6f9829b5997036ea94f41d79368c4e
tomskii <tom@subfusion.com>
2016-09-09 15:09:33 +0100

    add my files

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("start-page-live.html"); ?>
+
+<!-- This PHP file is a workaround for heroku. You cannot push an HTML, CSS and JS site to heroku.
+Adding a PHP file which effectively does nothing makes heroku treat your project as a PHP app.
+Nothing to see here, move along. -->
